8ba7dda028be21d54a8b90158a698a460a8751d4
HanlopDev <hanafbomar@gmail.com>
2023-11-14 09:01:37 +0300

    tweets

---
+++ Twitter Clone/View/Feed/TweetCellView.swift
@@ -0,0 +1,18 @@
+//
+//  TweetCellView.swift
+//  Twitter Clone
+//
+//  Created by Hanaf on 13/11/2023.
+//
+
+import SwiftUI
+
+struct TweetCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TweetCellView()
+}

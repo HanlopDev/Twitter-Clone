958da710271caf0c754de2481c8370158e4329bc
HanlopDev <hanafbomar@gmail.com>
2023-11-15 16:48:00 +0300

    searchBar Design

---
+++ Twitter Clone/View/Items/SearchBar.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBar.swift
+//  Twitter Clone
+//
+//  Created by Hanaf on 15/11/2023.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBar()
+}
---
+++ Twitter Clone/View/Search/SearchCell.swift
@@ -0,0 +1,18 @@
+//
+//  SearchCell.swift
+//  Twitter Clone
+//
+//  Created by Hanaf on 15/11/2023.
+//
+
+import SwiftUI
+
+struct SearchCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchCell()
+}

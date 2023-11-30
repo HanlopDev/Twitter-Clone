16d8983cd23029e5910318b3143912dcfc910ed4
HanlopDev <hanafbomar@gmail.com>
2023-11-30 15:00:56 +0300

    finalize slide menu

---
+++ Twitter Clone/View/Profile/UserProfile.swift
@@ -0,0 +1,18 @@
+//
+//  UserProfile.swift
+//  Twitter Clone
+//
+//  Created by Hanaf on 30/11/2023.
+//
+
+import SwiftUI
+
+struct UserProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserProfile()
+}

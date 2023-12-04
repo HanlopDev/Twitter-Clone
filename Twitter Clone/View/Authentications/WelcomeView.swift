cde9998a7573e29fb489e93b33e60a6a672349c8
HanlopDev <hanafbomar@gmail.com>
2023-12-04 13:02:38 +0300

    Auth UI

---
+++ Twitter Clone/View/Authentications/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  Twitter Clone
+//
+//  Created by Hanaf on 04/12/2023.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
---
+++ Twitter Clone/View/Profile/BlurView.swift
@@ -0,0 +1,8 @@
+//
+//  BlurView.swift
+//  Twitter Clone
+//
+//  Created by Hanaf on 30/11/2023.
+//
+
+import Foundation

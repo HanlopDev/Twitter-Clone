7092be82c13ae511b787392e7e214060de8da264
HanlopDev <hanafbomar@gmail.com>
2024-01-22 10:19:19 +0300

    registration View

---
+++ Twitter Clone/View/Items/CustomAuthTextField.swift
@@ -0,0 +1,18 @@
+//
+//  CustomAuthTextField.swift
+//  Twitter Clone
+//
+//  Created by Hanaf on 05/12/2023.
+//
+
+import SwiftUI
+
+struct CustomAuthTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomAuthTextField()
+}
---
+++ Twitter Clone/View/Items/SecureAuthTextField.swift
@@ -0,0 +1,18 @@
+//
+//  SecureAuthTextField.swift
+//  Twitter Clone
+//
+//  Created by Hanaf on 19/01/2024.
+//
+
+import SwiftUI
+
+struct SecureAuthTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecureAuthTextField()
+}
